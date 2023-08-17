70d616d411331f652224227c5ca72dda2a025374
MSI\leone <leonel.felezario@luckycue.tech>
2023-08-17 18:34:22 +0800

    define scrape method in grader class

--- lib/grader.rb
+++ lib/grader.rb
@@ -3,5 +3,19 @@ require "student"
 
 class Grader
   def initialize(file)
+    @file = file
+    @students = []
+  end
+
+  def scrape
+    CSV.foreach(@file, headers: true) do |row|
+      name = "#{row['first_name']} #{row['last_name']}"
+      english = row['english']
+      math = row['math']
+      physics = row['physics']
+      student = Student.new(name, english, math, physics)
+      @students << student
+    end
+    @students
   end
 end
