581cb41d6ed7fda76e3fd12b8171837e734dfa73
MSI\leone <leonel.felezario@luckycue.tech>
2023-08-17 18:38:56 +0800

    add the create and print the student table function

--- scrape.rb
+++ scrape.rb
@@ -4,8 +4,13 @@ require "terminal-table"
 file = File.open("./files/grades.csv")
 grader = Grader.new(file)
 
-table = Terminal::Table.new do |t|
-  t << ["Sample", "Output", "Here"]
+students = grader.scrape
+table_rows = []
+
+students.each do |student|
+  table_rows << [student.name, student.english.to_i.to_s, student.math.to_i.to_s, student.physics.to_i.to_s, student.average.to_i.to_s]
 end
+
+table = Terminal::Table.new(headings: ['Name', 'English', 'Math', 'Physics', 'Average'], rows: table_rows)
 
 puts table
