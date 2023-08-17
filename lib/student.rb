29dfa1c87c07181844993ac1403b3a4e234b5f13
MSI\leone <leonel.felezario@luckycue.tech>
2023-08-17 18:34:44 +0800

    define average method in student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,2 +1,14 @@
 class Student
+    attr_reader :name, :english, :math, :physics
+
+    def initialize(name, english, math, physics)
+      @name = name
+      @english = english.to_f
+      @math = math.to_f
+      @physics = physics.to_f
+    end
+  
+    def average
+      (@english + @math + @physics) / 3.0
+    end
 end
